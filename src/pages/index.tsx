cb471772f2b889b4ad34dab5f8aec34dcde040de
oginom <oginomaa@gmail.com>
2023-09-30 18:22:51 +0900

    fix visual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
 
+import rule1 from '../../public/rule1.png'
+import rule2 from '../../public/rule2.png'
+import rule3 from '../../public/rule3.png'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -17,7 +21,7 @@ export default function Home() {
           Play
         </Link>
       </div>
-      <div className="mb-2 grid lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-2 lg:mb-0 lg:grid-cols-4 lg:text-left">
         <h2 className={`mb-2 mt-6 text-2xl font-semibold`}>
           HOW TO PLAY
         </h2>
@@ -36,19 +40,19 @@ export default function Home() {
           <li className={`mt-4 mb-2`}>
             各プレイヤーは交互に自分の色の長方形のタイルを置いていきます。
             <div className="flex justify-center">
-              <Image src="/rule1.png" alt="1" width={300} height={300}/>
+              <Image src={rule1} alt="1" width={300} height={300}/>
             </div>
           </li>
           <li className={`mt-4 mb-2`}>
             タイルは盤面の格子に合わせて置きます。すでに置かれているタイルに被らなければどのような大きさでも構いません。
             <div className="flex justify-center">
-              <Image src="/rule2.png" alt="1" width={300} height={300}/>
+              <Image src={rule2} alt="1" width={300} height={300}/>
             </div>
           </li>
           <li className={`mt-4 mb-2`}>
             プレイヤーがタイルを置いて盤面が全て埋まったとき、そのプレイヤーの負けです。
             <div className="flex justify-center">
-              <Image src="/rule3.png" alt="1" width={300} height={300}/>
+              <Image src={rule3} alt="1" width={300} height={300}/>
             </div>
           </li>
         </ul>
